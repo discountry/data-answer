7c8b56179c413c676ea37ae07dc1c80ddbabe92a
discountry <331027793@qq.com>
2018-01-28 05:15:27 +0800

    animation

--- src/Pages/Reward.js
+++ src/Pages/Reward.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, WhiteSpace } from 'antd-mobile'
+import CountUp from 'react-countup'
 
 export default class Reward extends Component {
   render() {
@@ -12,7 +13,7 @@ export default class Reward extends Component {
         <div className="page-container reward">
           <h1>恭喜你获得</h1>
           <img className="token-icon" alt="token" src="https://png.icons8.com/nolan/128/000000/shekel.png" />
-          <p className="token-number">9,999 Token</p>
+          <p className="token-number"><CountUp start={0} end={999} /> Token</p>
           <WhiteSpace />
           <Button type="ghost" style={{ margin: '0 20px' }}>提现</Button>
         </div>
