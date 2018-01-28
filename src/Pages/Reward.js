4f8c8e4ce0cd2bc61f1dee9a7fd48c9e7203b1af
discountry <331027793@qq.com>
2018-01-28 08:18:47 +0800

    alert

--- src/Pages/Reward.js
+++ src/Pages/Reward.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import { Button, WhiteSpace } from 'antd-mobile'
 import CountUp from 'react-countup'
+import swal from 'sweetalert'
 
 export default class Reward extends Component {
+  voteForUs = () => {
+    swal({
+      title: "请为我们打Call!",
+      text: "Token 发了一定不会忘记您呐～",
+      icon: "success",
+    });
+  }
   render() {
     return (
       <div>
@@ -15,7 +23,7 @@ export default class Reward extends Component {
           <img className="token-icon" alt="token" src="https://png.icons8.com/nolan/128/000000/shekel.png" />
           <p className="token-number"><CountUp start={0} end={999} /> Token</p>
           <WhiteSpace />
-          <Button type="ghost" style={{ margin: '0 20px' }}>提现</Button>
+          <Button onClick={this.voteForUs} type="ghost" style={{ margin: '0 20px' }}>提现</Button>
         </div>
       </div>
     )
